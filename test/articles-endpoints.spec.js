c2f15055274772624e0ade180d086105838dd507
Olivia Lewis <ollybaby12@gmail.com>
2021-03-20 22:17:24 -0400

    refactor articles

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -4,7 +4,7 @@ const supertest = require('supertest')
 const app = require('../src/app')
 const { makeArticlesArray } = require('./articles.fixtures')
 
-describe.only(`Article Endpoints`, function() {
+describe(`Article Endpoints`, function() {
     let db
 
     before('make knex instance', () => {
@@ -77,6 +77,35 @@ describe.only(`Article Endpoints`, function() {
                     .get(`/articles/${articleId}`)
                     .expect(404, { error: { message: `Article doesn't exist`} })
             })
+        })
+    })
+
+    describe(`POST /articles`, () => {
+        it(`creates an article, responding with 201 and the new article`, function() {
+            this.retries(3)
+            const newArticle = {
+                title: 'Test new article',
+                style: 'Listicle',
+                content: 'Test new article content'
+            }
+            return supertest(app)
+                .post('/articles')
+                .send(newArticle)
+                .expect(res => {
+                    expect(res.body.title).to.eql(newArticle.title)
+                    expect(res.body.style).to.eql(newArticle.style)
+                    expect(res.body.content).to.eql(newArticle.content)
+                    expect(res.body).to.have.property('id')
+                    expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+                    const expected = new Date().toLocaleString()
+                    const actual = new Date(res.body.date_published).toLocaleString()
+                    expect(actual).to.eql(expected)
+                })
+                .then(postRes =>
+                    supertest(app)
+                        .get(`/articles/${postRes.body.id}`)
+                        .expect(postRes.body)
+                )
         })
     })
 })
